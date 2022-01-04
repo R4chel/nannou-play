4502abdf4ced0f23b29d95ed0970d75ded1c3859
R4chel <rae84@cornell.edu>
2022-01-04 16:05:59 -0500

    draw a line

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,19 @@ fn model(_app: &App) -> Model {
 
 fn update(_app: &App, _model: &mut Model, _update: Update) {}
 
-fn view(_app: &App, _model: &Model, frame: Frame) {
-    frame.clear(PURPLE);
+fn view(app: &App, _model: &Model, frame: Frame) {
+    // Prepare to draw.
+    let draw = app.draw();
+
+    // Clear the background to purple.
+    draw.background().color(PLUM);
+
+    let points = (0..50).map(|i| {
+        let x = i as f32 - 25.0; //subtract 25 to center the sine wave
+        let point = pt2(x, x.sin()) * 20.0; //scale sine wave by 20.0
+        (point, STEELBLUE)
+    });
+    draw.polyline().weight(3.0).points_colored(points);
+
+    draw.to_frame(app, &frame).unwrap();
 }
