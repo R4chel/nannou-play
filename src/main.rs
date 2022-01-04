d07c6fe003cf7ae41ef1f96d903f48c3078f4bc1
R4chel <rae84@cornell.edu>
2022-01-04 16:11:55 -0500

    playing

--- src/main.rs
+++ src/main.rs
@@ -26,5 +26,12 @@ fn view(app: &App, _model: &Model, frame: Frame) {
     });
     draw.polyline().weight(3.0).points_colored(points);
 
+    let points = (0..500).map(|i| {
+        let x = i as f32 / 10.0 - 25.0; //subtract 25 to center the sine wave
+        let point = pt2(x, (x).cos()) * 20.0; //scale sine wave by 20.0
+        (point, STEELBLUE)
+    });
+    draw.polyline().weight(3.0).points_colored(points);
+
     draw.to_frame(app, &frame).unwrap();
 }
